38933b558a58518d3f4ec8f9eee3dee05a5a7753
缪宇 <miaoyu2009@qq.com>
2024-02-22 19:41:35 +0800

    feat: deep clone

---
+++ src/__test__/deepClone.test.js
@@ -0,0 +1,44 @@
+import { deepClone } from "..";
+import { describe, test, expect } from "vitest"
+
+describe('deep clone', () => {
+
+    test('base', () => {
+
+        const obj = { a: {}, b: [], c: 1 }
+        const newObj = deepClone(obj)
+
+        console.log("====", obj, newObj)
+        expect(newObj === obj).toBe(false)
+        expect(newObj.c === obj.c).toBe(true)
+        expect(newObj.a === obj.a).toBe(false)
+        expect(newObj.b === obj.b).toBe(false)
+    });
+
+    test('obj', () => {
+
+        const obj = { a: 1, b: { c: {}, d: 1 } }
+        const newObj = deepClone(obj)
+
+        expect(newObj === obj).toBe(false)
+        expect(newObj.b.c === obj.b.c).toBe(false)
+        expect(newObj.b.d === obj.b.d).toBe(true)
+    });
+
+
+    test('array', () => {
+        const obj = { a: 1, b: { c: [], d: 1 } }
+        const newObj = deepClone(obj)
+        expect(newObj.b.c === obj.b.c).toBe(false)
+
+    });
+
+
+    test('loop nest', () => {
+        let g = {h: {}}
+        const obj = { b: { f: g } }
+        g.h = obj.b
+        const newObj = deepClone(obj)
+        expect(newObj.b.f === obj.b.f).toBe(false)
+    });
+});
