3434c1dc68efbae1a8873041007dea04e79b960e
缪宇 <miaoyu2009@qq.com>
2024-02-22 20:04:29 +0800

    feat: deepFirst and breadthFirst

---
+++ src/__test__/loop.test.js
@@ -0,0 +1,34 @@
+import { deepFirst, breadthFirst } from "..";
+import { describe, test, expect } from "vitest"
+
+const tree = {
+    id: 0,
+    children: [
+        {
+            id: 1, children: [
+                { id: 3, children: [] },
+            ]
+        },
+        {
+            id: 2, children: [
+
+                { id: 4, children: [] },
+            ]
+        },
+    ]
+}
+describe('loop', () => {
+
+    test('deepFirst', () => {
+
+        expect(deepFirst([tree])).toEqual([3, 1, 4, 2, 0])
+
+    });
+
+    test('breadthFirst', () => {
+
+        expect(breadthFirst([tree])).toEqual([0, 1, 2, 3, 4])
+    });
+
+
+});
