d1ac645c2c46345b2934ccb5454e708632f48e08
缪宇 <miaoyu2009@qq.com>
2024-02-27 17:06:17 +0800

    feat: flat

--- src/__test__/algorithm.test.js
+++ src/__test__/algorithm.test.js
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "vitest"
-import { breadthFirst, deepFirst } from "../algorithm";
+import { breadthFirst, deepFirst, flat } from "../algorithm";
 
 const tree = {
     id: 0,
@@ -17,7 +17,7 @@ const tree = {
         },
     ]
 }
-describe('loop', () => {
+describe('algorithm', () => {
 
     test('deepFirst', () => {
 
@@ -30,5 +30,10 @@ describe('loop', () => {
         expect(breadthFirst([tree])).toEqual([0, 1, 2, 3, 4])
     });
 
+    test('编写一个程序将数组扁平化去并除其中重复部分数据，最终得到一个升序且不重复的数组', () => {
+        var arr = [[1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14]]]], 10];
+
+        expect(flat(arr)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
+    })
 
 });
