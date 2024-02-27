dd9c8e4861b48e37e47bdc07706264ad3f569bab
缪宇 <miaoyu2009@qq.com>
2024-02-27 15:24:40 +0800

    feat: promise

--- src/async.js
+++ src/async.js
@@ -16,27 +16,99 @@ Async/Await 就行一个自执行的 generate 函数，yield 可以想象成 awa
  * 异步考察
  */
 
-async function async1() {
-    console.log('async1 start');
-    await async2();
-    console.log('async1 end');
-}
-async function async2() {
-    console.log('async2');
-}
-console.log('script start');
-setTimeout(function () {
-    console.log('setTimeout');
-}, 0)
-async1();
-new Promise(function (resolve) {
-    console.log('promise1');
-    resolve();
-}).then(function () {
-    console.log('promise2');
-});
-console.log('script end');
+// async function async1() {
+//     console.log('async1 start');
+//     await async2();
+//     console.log('async1 end');
+// }
+// async function async2() {
+//     console.log('async2');
+// }
+// console.log('script start');
+// setTimeout(function () {
+//     console.log('setTimeout');
+// }, 0)
+// async1();
+// new Promise(function (resolve) {
+//     console.log('promise1');
+//     resolve();
+// }).then(function () {
+//     console.log('promise2');
+// });
+// console.log('script end');
 
 /**
- * 手写promise
+ * 手写Promise
+ * 
+ * Promise.all
+ * Promise.race
  */
+
+// 使用示例
+const promise = new Promise((resolve, reject) => {
+    resolve(42);
+});
+
+promise.then(value => {
+    console.log('Success:', value); // 输出 Success: 42
+}).then(() => {
+    console.log('Chain success handler');
+}).then(() => {
+    throw new Error('Custom error');
+}).then(() => {
+    console.log('This should not run');
+}, reason => {
+    console.error('Error:', reason); // 输出 Error: Error: Custom error
+});
+
+class MyPromise {
+    onFulfilled
+    onRejected
+    constructor(executor) {
+        if (executor) {
+            executor(this.resolve.bind(this), this.reject.bind(this))
+        }
+    }
+
+    resolve(...args) {
+        setTimeout(() => {
+            if (this.onFulfilled) {
+                this.onFulfilled(...args)
+            }
+        })
+
+    }
+    reject(...args) {
+        setTimeout(() => {
+            if (this.onRejected) {
+                this.onRejected(...args)
+            }
+        })
+    }
+
+}
+
+MyPromise.prototype.then = function (onFulfilled, onRejected) {
+    this.onFulfilled = onFulfilled
+    this.onRejected = onRejected
+    return new MyPromise((resolve, reject) => {
+        resolve()
+    })
+}
+
+
+const p1 = new MyPromise((resolve, reject) => {
+    resolve(42);
+});
+
+p1.then(value => {
+    console.log('Success:', value); // 输出 Success: 42
+}).then(() => {
+    console.log('Chain success handler');
+}).then(() => {
+    throw new Error('Custom error');
+}).then(() => {
+    console.log('This should not run');
+}, reason => {
+    console.error('Error:', reason); // 输出 Error: Error: Custom error
+});
