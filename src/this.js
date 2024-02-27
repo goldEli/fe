c6cd224b2e96d1960b1297650bce2d20e64478ea
缪宇 <miaoyu2009@qq.com>
2024-02-27 10:56:58 +0800

    feat: this

--- src/this.js
+++ src/this.js
@@ -39,6 +39,42 @@ Function.prototype.myApply = function (context, args = []) {
 }
 
 
+
+
+
 /**
- * 手写 new
+ * this 考察
+ 
+var name = 'window'
+
+var person1 = {
+  name: 'person1',
+  show1: function () {
+    console.log(this.name)
+  },
+  show2: () => console.log(this.name),
+  show3: function () {
+    return function () {
+      console.log(this.name)
+    }
+  },
+  show4: function () {
+    return () => console.log(this.name)
+  }
+}
+var person2 = { name: 'person2' }
+
+person1.show1()               // person1
+person1.show1.call(person2)   // person2
+
+person1.show2()               // window
+person1.show2.call(person2)   // window 箭头函数没有this
+
+person1.show3()()             // window
+person1.show3().call(person2) // person2
+person1.show3.call(person2)() // window
+
+person1.show4()()             //person1
+person1.show4().call(person2) //person1
+person1.show4.call(person2)() //person2
  */
